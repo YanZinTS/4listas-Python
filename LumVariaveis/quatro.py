4e0b3f031e572235c7d31dd129721181886a76ea
Yanzin <yantsantana155@gmail.com>
2023-04-10 14:06:46 -0300

    first commit

--- LumVariaveis/quatro.py
+++ LumVariaveis/quatro.py
@@ -1,7 +1,7 @@
-num = input("Digite um número com no máximo 3 digitos: ")
-centena = num[0]
-dezena = num[1]
-unidade = num[2]
+cdu = input("Digite um número com no máximo 3 digitos: ")
+centena = cdu[0]
+dezena = cdu[1]
+unidade = cdu[2]
 
 print(f"Centena = {centena}")
 print(f"Dezena = {dezena}")
